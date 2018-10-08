697c9dcf06b3446814c32215ce52b256fa41e0f1
Oskar Dubas <14065312@students.southwales.ac.uk>
2018-10-08 13:28:40 +0100

    player and camera controller

---
+++ McompProject/Assets/Scripts/CamerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerController : MonoBehaviour {
+
+    public GameObject player;
+
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+
+        offset = transform.position - player.transform.position;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = player.transform.position + offset;
+
+	}
+}
---
+++ McompProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Rigidbody Rigidbody;
+    private Camera mainCamera;
+    float rayLength;
+
+    // Use this for initialization
+    void Start () {
+
+        Rigidbody = GetComponent<Rigidbody>();
+
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); //check whats better raw or not
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        
+        //check if ray hit plane
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 aimAt = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, aimAt, Color.green);
+
+            transform.LookAt(new Vector3(aimAt.x, transform.position.y, aimAt.z));
+
+        }
+
+	}
+
+    void FixedUpdate()
+    {
+        Rigidbody.velocity = moveVelocity;
+    }
+
+
+}
