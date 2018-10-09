b7951cb0385f5657fe64c81facec59d3b60c4af4
Oskar Dubas <14065312@students.southwales.ac.uk>
2018-10-09 10:19:46 +0100

    Small fix, typo and new scene for testing shooting

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamerController : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
     public GameObject player;
 
