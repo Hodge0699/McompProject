9f9be390b3b380fd29e55b05173b5b7aee599a85
Hodge <jackh625@hotmail.co.uk>
2018-11-07 09:16:52 +0000

    shotgun part one

---
+++ Assets/Powerups/PowerupSG.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSG : MonoBehaviour {
+    public float time = 5.0f;
+    public GunController GC;
+    public float BulletSpeed = 0.25f;
+    public float Duration = 30f;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+         Debug.Log("ShotGun Pickup");   
+        }
+           
+    }
+
+    private void Reset()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+           
+           
+        }
+        time = 5.0f;
+    }
+}
