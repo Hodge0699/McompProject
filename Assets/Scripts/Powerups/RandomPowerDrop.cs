07f49b937b0af9b2db522bfdc59692c95adb365a
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2018-11-08 14:49:33 +0000

    Random powerupdrop
    moved power up scripts in correct folder
    
    created script for random weapon drop

---
+++ Assets/Scripts/Powerups/RandomPowerDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPowerDrop : MonoBehaviour {
+
+    public powerupSG1 Shotgun;
+    public PowerupMG MachineGun;
+    public PowerupsEXD ExtraDamage;
+ 
+	
+    public class ItemDrops
+    {
+        
+    }
+
+    public List<ItemDrops> ItemTable = new List<ItemDrops>();
+    public int ItemDropChance;
+
+
+    public  void DropRate()
+    {
+       int calc_dropRate = Random.Range(0, 101);
+       if(calc_dropRate > ItemDropChance)
+        {
+            Debug.Log("No powerups");
+            return;
+        }
+      
+      
+    }
+
+}
