ada85b1935130aa6579eee9e2b5d06c298f7322a
Liam Vallance <14014610@students.southwales.ac.uk>
2018-11-12 00:06:29 +0000

    Enemies Spawning per room
    Enemies can now only spawn within the bounds of the current room and the spawn bounds are adjusted to fit the size of the room without spawning enemies too close to walls and dors
    TODO: stop enemies from spawning too close to the player, make amound of enemies dependent on room size, have enemies spawn when new room is entered

--- Assets/Scripts/AI/EnemiesSpawn.cs
+++ Assets/Scripts/AI/EnemiesSpawn.cs
@@ -5,55 +5,60 @@ using UnityEngine;
 //Script needs to be connect to an empty object
 //Make sure gizmos are visible!
 
-public class EnemiesSpawn : MonoBehaviour
+namespace Enemy
 {
+	public class EnemiesSpawn : MonoBehaviour
+	{
+	    public Vector3 center;
+	    public Vector3 size;
 
-    public Vector3 center;
-    public Vector3 size;
+	    public GameObject Enemyprefab;
+	    public GameObject floor;
 
-    public GameObject Enemyprefab;
-    public GameObject floor;
+	    // Use this for initialization
+	    void Start()
+	    {
+	        //Spawn();
+	    }
 
-    // Use this for initialization
-    void Start()
-    {
-        //Spawn();
-    }
+	    // Update is called once per frame
+	    void Update()
+	    {
+	        if (Input.GetKey(KeyCode.O))
+	        {
+	            SpawnBounds();
+	        }
+	    }
 
-    // Update is called once per frame
-    void Update()
-    {
+	    public void SpawnBounds()
+	    {
+	    	floor = GameObject.Find("Floor");
 
+	    	center = floor.transform.position;
+	    	center.y = center.y + 1;
 
-        if (Input.GetKey(KeyCode.O))
-        {
-            Spawn();
-        }
+	        size = floor.GetComponent<Renderer>().bounds.size;
+	        size.x = size.x - 5;
+	        size.z = size.z - 5;
 
-    }
+	        Spawn();
+	    }
 
-    public void Spawn()
-    {
+	    void Spawn()
+	    {
+	        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
 
-    	 floor = GameObject.Find("Floor");
-
-    	center = floor.transform.position;
-    	center.y = center.y + 1;
-
-        size = floor.GetComponent<Renderer>().bounds.size;
-
-        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
-
-        // Vector2 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.z / 2, size.z / 2));
-
-        Instantiate(Enemyprefab, pos, Quaternion.identity);
-    }
+	        // Vector2 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.z / 2, size.z / 2));
+	        
+	        Instantiate(Enemyprefab, pos, Quaternion.identity);
+	    }
 
 
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawCube(center, size);
-        Gizmos.DrawCube(transform.localPosition, size);
-    }
+	    void OnDrawGizmosSelected()
+	    {
+	        Gizmos.color = new Color(1, 0, 0, 0.5f);
+	        Gizmos.DrawCube(center, size);
+	        Gizmos.DrawCube(transform.localPosition, size);
+	    }
+	}
 }
