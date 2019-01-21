dba165d5b602f8b324b848497ec00e4d8d8cbe61
Liam Vallance <14014610@students.southwales.ac.uk>
2019-01-21 19:44:26 +0000

    UI time mechanic used indicator
    Added a UI element that shows what time mechanic the player has and how to use it

---
+++ Assets/Scripts/TimeUsedUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimeUsedUI : MonoBehaviour {
+
+    Scene scene;
+    public Text txt;
+
+    private void OnGUI()
+    {
+        if (scene.name == "LevelTwo")
+        {
+            txt.text = "Time Mechanic: Rewind\nUse: Space";
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
