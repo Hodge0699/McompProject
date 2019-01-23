37b32e6325e139f84804526bde881540a969c4f1
Nicky <14016036@students.southwales.ac.uk>
2019-01-23 09:02:15 +0000

    Fixed boss prefab & edited the teleport mechanic better
    ited the boss prefab & have further edited the teleport script but will require further improvements/adjustments for different teleport styles

--- Assets/Scripts/AI/Enemy Types/BossMechanics/BossTeleportM.cs
+++ Assets/Scripts/AI/Enemy Types/BossMechanics/BossTeleportM.cs
@@ -7,59 +7,49 @@ using RoomBuilding;
 
 public class BossTeleportM : MonoBehaviour {
 
-<<<<<<< HEAD
-    [SerializeField]
-    List<GameObject> teleportLocations;
-    private BossEnemy bEnemy;
     [SerializeField]
     private Room myRoom;
     private bool stopChecking = false;
-=======
+    [SerializeField]
     private List<GameObject> teleportLocations = new List<GameObject>();
     private BossEnemy bEnemy;
->>>>>>> master
+    [SerializeField]
+    private bool randTeleport = false;
+    private bool teleport = true;
 
     private void Start()
     {
-<<<<<<< HEAD
         // This will find every teleport location in the scene (including other rooms since multiple boss
         // initially spawn before the player decideds which one to walk into), fix this - Jake - suck me - Nicky 
+        bEnemy = this.GetComponent<BossEnemy>();
         myRoom = bEnemy.getMyRoom();
-        foreach (GameObject teleportlocations in myRoom.transform.Find("BossTeleporter"))
-        {
-            teleportLocations.Add(teleportlocations);
-        }
 
-        bEnemy = this.GetComponent<BossEnemy>();
-    }
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log("Teleport Location Capacity " + teleportLocations.Capacity);
-        if (bEnemy.currentHealth < 800)
-        {
-            int i = Random.Range(0, 3);
-            this.transform.position = teleportLocations[i].transform.position;
-        }
-        
-
-	}
-
-    void checkForErrors()
-    {
-=======
-        bEnemy = this.GetComponent<BossEnemy>();
->>>>>>> master
-
-        // Code I told Nicky to type while making nuggets that ended up not working because he had that ^ line
-        // below it and it was called in Awake instead of Start. So I ended up doing it on my branch and 
-        // fixing it but he wanted a mention anyway - Jake.
         Transform teleContainer = bEnemy.getRoom().transform.Find("BossTeleporter").transform;
         for (int i = 0; i < teleContainer.childCount; i++)
             teleportLocations.Add(teleContainer.GetChild(i).gameObject);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(randTeleport == true)
+            randomTeleport();
+        
+
+	}
+    /// <summary>
+    /// randomly teleports the boss to any of the teleport locations specified.
+    /// </summary>
+    private void randomTeleport()
+    {
+        if (bEnemy.currentHealth < 700)
+        {
+            if (teleport == true)
+            {
+                int i = Random.Range(0, teleportLocations.Capacity);
+                this.transform.position = teleportLocations[i].transform.position;
+            }
+        }
+        teleport = false;
     }
 }
