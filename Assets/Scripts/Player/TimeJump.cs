41c43a2de630b9aa2ad6742dc9e581aa78cd74c5
Oskar Dubas <14065312@students.southwales.ac.uk>
2019-02-03 21:27:44 +0000

    new scene...

---
+++ Assets/Scripts/Player/TimeJump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeJump : MonoBehaviour {
+
+
+    private float distance = 500;
+    private bool teleported;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            
+            teleported = !teleported;
+            
+            Vector3 pos = gameObject.transform.position;
+
+            if (teleported == true)
+            {
+                
+                gameObject.transform.position = new Vector3(pos.x + distance, pos.y, pos.z);
+            }
+            else
+            {
+                
+                gameObject.transform.position = new Vector3(pos.x - distance, pos.y, pos.z);
+            }
+
+
+        }
+}
