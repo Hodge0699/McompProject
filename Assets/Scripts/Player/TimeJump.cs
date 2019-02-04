8c517648b093b48e58d3324f5decb5fd699c5e62
Oskar Dubas <14065312@students.southwales.ac.uk>
2019-02-04 21:46:04 +0000

    Time Travel script (simple version for testing)

--- Assets/Scripts/Player/TimeJump.cs
+++ Assets/Scripts/Player/TimeJump.cs
@@ -2,40 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeJump : MonoBehaviour {
+public class TimeJump : MonoBehaviour
+{
 
 
-    private float distance = 500;
+    private float distance = 100;
     private bool teleported;
 
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
 
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            
+
             teleported = !teleported;
-            
+
             Vector3 pos = gameObject.transform.position;
 
             if (teleported == true)
             {
-                
+
                 gameObject.transform.position = new Vector3(pos.x + distance, pos.y, pos.z);
             }
             else
             {
-                
+
                 gameObject.transform.position = new Vector3(pos.x - distance, pos.y, pos.z);
             }
 
 
         }
+    }
 }
