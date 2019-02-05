47c2d229374cf3b3b0549e0fa34b327dab506415
Nethernights <14016036@students.southwales.ac.uk>
2019-02-05 12:41:13 +0000

    Master fix (hopefully)

---
+++ Assets/Scripts/TimeStop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStop : MonoBehaviour
+{
+
+    [SerializeField]
+
+    bool pausetime = false;
+
+    public float PauseTime = 0.0f;
+    private float NonPausedTime = 1.0f;
+    
+     void Start()
+    {
+       
+    }
+
+
+    public void Update()
+    {
+       if(Input.GetKeyDown("space"))
+        {
+       
+            StartPaused(); 
+        }
+       else
+        if(Input.GetKeyUp("space"))
+        {
+            EndPaused();
+        }
+    }
+
+
+    void EndPaused()
+    {
+        Time.timeScale = NonPausedTime;
+    }
+    void StartPaused()
+    {
+        
+        pausetime = true;
+        Time.timeScale = PauseTime;
+        
+    }
+}
