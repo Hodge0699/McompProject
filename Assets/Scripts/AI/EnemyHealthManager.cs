ca28ccedf7372c1fb092e7dd40e6badd1dc3bdbf
Nethernights <14016036@students.southwales.ac.uk>
2019-03-14 00:01:24 +0000

    Updated Textures, Added enemy death animation and fixed slow down time mechanic that got broken

--- Assets/Scripts/AI/EnemyHealthManager.cs
+++ Assets/Scripts/AI/EnemyHealthManager.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+using EnemyType;
+
 public class EnemyHealthManager : HealthManager {
 
     [Header("Enemy Health")]
     public Image healthBar;
 
-    public bool lookAtPlayerOnHit = true;
 
+    public bool lookAtPlayerOnHit = true;
+    private float deathAnimationDuration = 6;
+
+
+    new void Update()
+    {
+
+        if (deathAnimationDuration <= 2)
+        {
+            Debug.Log("Timer: " + deathAnimationDuration);
+            deathAnimationDuration -= Time.deltaTime;
+        }
+        if (deathAnimationDuration <= 0)
+            destroyGameObject();
+    }
+
+    /// <summary>
+    /// Enemy takes damage
+    /// </summary>
+    /// <param name="damageAmount"></param>
     public override void hurt(float damageAmount)
     {
         if (!isAlive)
@@ -46,7 +67,14 @@ public class EnemyHealthManager : HealthManager {
     /// </summary>
     private void die()
     {
+        anim.SetTrigger("Dead");
         EnemyType.AbstractEnemy me = GetComponent<EnemyType.AbstractEnemy>();
+        
+        if(GetComponent<GunEnemy>() != null)
+            Destroy(GetComponent<GunEnemy>());
+        if(GetComponent<MeleeEnemy>() != null)
+            Destroy(GetComponent<MeleeEnemy>());
+
 
         if (me != null)
         {
@@ -59,6 +87,12 @@ public class EnemyHealthManager : HealthManager {
             me.onDeath();
         }
 
+        deathAnimationDuration = 2;
+    }
+
+    private void destroyGameObject()
+    {
+        Debug.Log("something");
         Destroy(gameObject);
     }
 }
