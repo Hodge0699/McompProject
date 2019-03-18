20c9f048abffb4899bc4b850dd037b1f949a6252
Jake Passmore <14734231@students.southwales.ac.uk>
2019-03-18 18:07:14 +0000

    Added controls to hats scene, created another Darren without PlayerController and with HatAnchor

---
+++ Assets/Scripts/Player/Runway/RunwayCameraOrbiter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Runway
+{
+    public class RunwayCameraOrbiter : MonoBehaviour
+    {
+
+        public float rotationSpeed = 50.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                transform.RotateAround(Vector3.zero, Vector3.right, -rotationSpeed * Time.deltaTime);
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                transform.RotateAround(Vector3.zero, Vector3.right, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Runway/RunwayModelDarren.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Runway
+{
+    public class RunwayModelDarren : MonoBehaviour
+    {
+        public GameObject hat;
+        public float rotationSpeed = 50.0f;
+
+        private void Start()
+        {
+            GameObject h = Instantiate(hat);
+            h.transform.SetParent(transform.Find("Hat Anchor"), false);
+            h.name = "Hat";
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}
