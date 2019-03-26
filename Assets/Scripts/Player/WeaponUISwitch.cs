6eff0ca7ab83823e6d270f759761e174428600b1
LiamVallance <14014610@students.southwales.ac.uk>
2019-03-26 14:09:53 +0000

    Added weapon ammo count to weapon UI

--- Assets/Scripts/Player/WeaponUISwitch.cs
+++ Assets/Scripts/Player/WeaponUISwitch.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using Weapon.Gun;
+
 public class WeaponUISwitch : MonoBehaviour {
 
     [SerializeField] private Image handgun;
@@ -11,8 +13,16 @@ public class WeaponUISwitch : MonoBehaviour {
     //[SerializeField] private Image EXDhandgun;
     //[SerializeField] private Image nonTimeEffectingGun;
 
+    [SerializeField] private Transform AmmoUI;
+
+    private int currentAmmo = 125;
+
     List<Image> weaponUI = new List<Image>();
     int currentWeapon;
+
+    public Handgun handgunClass;
+    public Shotgun shotgunClass;
+    public MachineGun machinegunClass;
 
     private void initWeaponUI()
     {
@@ -23,22 +33,53 @@ public class WeaponUISwitch : MonoBehaviour {
         //weaponUI.Add(nonTimeEffectingGun);
     }
 
-    // Use this for initialization
-    void Start () {
-        weaponUI.Add(handgun);
-        weaponUI.Add(shotgun);
-        weaponUI.Add(machinegun);
-        //weaponUI.Add(EXDhandgun);
-        //weaponUI.Add(nonTimeEffectingGun);
+    private void Awake()
+    {
+        handgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Handgun>();
+        shotgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Shotgun>();
+        machinegunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<MachineGun>();
 
-        // Show starting pistol - Jake
-        switchWeaponUI(0);
+    }
+
+        // Use this for initialization
+        void Start () {
+    weaponUI.Add(handgun);
+    weaponUI.Add(shotgun);
+    weaponUI.Add(machinegun);
+    //weaponUI.Add(EXDhandgun);
+    //weaponUI.Add(nonTimeEffectingGun);
+
+    // Show starting pistol - Jake
+    switchWeaponUI(0);
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+
+        if (handgunClass == null)
+            handgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Handgun>();
+
+        if (shotgunClass == null)
+            shotgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Shotgun>();
+
+        if (machinegunClass == null)
+            machinegunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<MachineGun>();
+
+        if (weaponUI[0].isActiveAndEnabled)
+            currentAmmo = handgunClass.getCurrentAmmo();
+        if (weaponUI[1].isActiveAndEnabled)
+            currentAmmo = shotgunClass.getCurrentAmmo();
+        if (weaponUI[2].isActiveAndEnabled)
+            currentAmmo = machinegunClass.getCurrentAmmo();
+
+        updateAmmo(currentAmmo);
+
+    }
+
+    public void updateAmmo(int ammo)
+    {
+        AmmoUI.GetComponent<Text>().text = ammo.ToString();
+    }
 
 
     public void switchWeaponUI(int i)
