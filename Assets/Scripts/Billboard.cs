5fb7b09a4873cc6d02f0284abbdf674606f2270d
Liam Vallance <14014610@students.southwales.ac.uk>
2019-03-29 18:51:04 +0000

    Added weapon graphics for ammo pickup
    Replaced the coloured boxes for the weapon ammo pickups with floating bobbing version of the weapons UI graphics to better represent what they player is picking up

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    public GameObject cam;
+    
+    private void Update()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        transform.rotation = cam.transform.rotation;
+    }
+}
--- Assets/Scripts/Player/WeaponUISwitch.cs
+++ Assets/Scripts/Player/WeaponUISwitch.cs
@@ -10,7 +10,7 @@ public class WeaponUISwitch : MonoBehaviour {
     [SerializeField] private Image handgun;
     [SerializeField] private Image shotgun;
     [SerializeField] private Image machinegun;
-    //[SerializeField] private Image EXDhandgun;
+    [SerializeField] private Image EXDhandgun;
     //[SerializeField] private Image nonTimeEffectingGun;
 
     [SerializeField] private Transform AmmoUI;
@@ -23,34 +23,36 @@ public class WeaponUISwitch : MonoBehaviour {
     public Handgun handgunClass;
     public Shotgun shotgunClass;
     public MachineGun machinegunClass;
+    public EXDHandgun EXDHandgunClass;
 
     private void initWeaponUI()
     {
         weaponUI.Add(handgun);
         weaponUI.Add(shotgun);
         weaponUI.Add(machinegun);
-        //weaponUI.Add(EXDhandgun);
+        weaponUI.Add(EXDhandgun);
         //weaponUI.Add(nonTimeEffectingGun);
     }
 
-    private void Awake()
-    {
-        handgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Handgun>();
-        shotgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Shotgun>();
-        machinegunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<MachineGun>();
+    //private void Awake()
+    //{
+    //    handgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Handgun>();
+    //    shotgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Shotgun>();
+    //    machinegunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<MachineGun>();
+    //    EXDHandgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<EXDHandgun>();
+    //
+    //}
 
-    }
+    // Use this for initialization
+    void Start () {
+        weaponUI.Add(handgun);
+        weaponUI.Add(shotgun);
+        weaponUI.Add(machinegun);
+        weaponUI.Add(EXDhandgun);
+        //weaponUI.Add(nonTimeEffectingGun);
 
-        // Use this for initialization
-        void Start () {
-    weaponUI.Add(handgun);
-    weaponUI.Add(shotgun);
-    weaponUI.Add(machinegun);
-    //weaponUI.Add(EXDhandgun);
-    //weaponUI.Add(nonTimeEffectingGun);
-
-    // Show starting pistol - Jake
-    switchWeaponUI(0);
+        // Show starting pistol - Jake
+        switchWeaponUI(0);
     }
 
     // Update is called once per frame
@@ -58,12 +60,15 @@ public class WeaponUISwitch : MonoBehaviour {
 
         if (handgunClass == null)
             handgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Handgun>();
-
+        
         if (shotgunClass == null)
             shotgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<Shotgun>();
-
+        
         if (machinegunClass == null)
             machinegunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<MachineGun>();
+        
+        if (EXDHandgunClass == null)
+            EXDHandgunClass = GameObject.FindGameObjectWithTag("Player").transform.Find("GunPrimary").GetComponent<EXDHandgun>();
 
         if (weaponUI[0].isActiveAndEnabled)
             currentAmmo = handgunClass.getCurrentAmmo();
@@ -71,6 +76,8 @@ public class WeaponUISwitch : MonoBehaviour {
             currentAmmo = shotgunClass.getCurrentAmmo();
         if (weaponUI[2].isActiveAndEnabled)
             currentAmmo = machinegunClass.getCurrentAmmo();
+        if (weaponUI[3].isActiveAndEnabled)
+            currentAmmo = EXDHandgunClass.getCurrentAmmo();
 
         updateAmmo(currentAmmo);
 
