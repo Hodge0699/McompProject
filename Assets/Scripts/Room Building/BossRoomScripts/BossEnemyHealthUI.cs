bbbac06fdd212022254f081193ab484db8472244
Nethernights <14016036@students.southwales.ac.uk>
2019-04-17 09:59:08 +0100

    changed first boss to use updated models, and created new script to have it fully functioning

---
+++ Assets/Scripts/Room Building/BossRoomScripts/BossEnemyHealthUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemyHealthUI : MonoBehaviour {
+
+    private GameObject player;
+    private GameObject BossUI;
+	// Update is called once per frame
+	void Update () {
+		if(player == null)
+        {
+            player = GameObject.Find("Player");
+            BossUI = player.transform.Find("BossHealth").gameObject;
+            BossUI.SetActive(true);
+        }
+	}
+}
