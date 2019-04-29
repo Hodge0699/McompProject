eec6519289a6ef3e52ed08a8433f54e88c4857bf
JakeP121 <14734231@students.southwales.ac.uk>
2019-04-29 18:55:16 +0100

    Bullets now affected by speedup

--- Assets/Scripts/Shooting/BulletController.cs
+++ Assets/Scripts/Shooting/BulletController.cs
@@ -38,7 +38,7 @@ public class BulletController : MonoBehaviour {
         if(!timeEffected)
             transform.Translate(Vector3.forward * speed * Time.unscaledDeltaTime);
         else
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * speed * myTime.getLocalDelta());
 
     }
 
