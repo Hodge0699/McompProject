f5ed7296e2d477112864746f9afabe2781a676de
Liam Vallance <14014610@students.southwales.ac.uk>
2019-05-01 18:09:16 +0100

    Buttons Animations
    Basic menu button animations implemented

---
+++ Assets/Menu Assets/MenuButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour {
+
+    [SerializeField] MenuButtonController menuButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(menuButtonController.index == thisIndex)
+        {
+            animator.SetBool("selected", true);
+        }
+        else
+        {
+            animator.SetBool("selected", false);
+        }
+        
+
+	}
+}
---
+++ Assets/Menu Assets/MenuButtonController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour {
+
+    public int index;
+    [SerializeField] bool keydown;
+    [SerializeField] int maxIndex;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if (!keydown)
+            {
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (index < maxIndex)
+                        index++;
+                    else
+                        index = 0;
+                }
+                else if (Input.GetAxis("Vertical") > 0)
+                {
+                    if (index > 0)
+                        index--;
+                    else
+                        index = maxIndex;
+                }
+            }
+            keydown = true;
+        }else
+            keydown = false;
+	}
+}
