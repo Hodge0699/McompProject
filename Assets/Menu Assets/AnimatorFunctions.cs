1eba48d44d8b7e8056f51006d1b0b49fb058e001
Liam Vallance <14014610@students.southwales.ac.uk>
2019-05-01 22:53:11 +0100

    Further Menu Enhancements
    Improved button a select and interact animations, started reimplementng button functions, Quit working both mouse and keyboard

--- Assets/Menu Assets/AnimatorFunctions.cs
+++ Assets/Menu Assets/AnimatorFunctions.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimatorFunctions : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] MenuButtonController menuButtonController;
+    public Button btn;
+
+    void OnPressed()
+    {
+        btn.onClick.Invoke();
+    }
 }
--- Assets/Menu Assets/MenuButton.cs
+++ Assets/Menu Assets/MenuButton.cs
@@ -1,31 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MenuButton : MonoBehaviour {
+public class MenuButton : MonoBehaviour, IPointerEnterHandler
+{
 
     [SerializeField] MenuButtonController menuButtonController;
     [SerializeField] Animator animator;
     [SerializeField] AnimatorFunctions animatorFunctions;
     [SerializeField] int thisIndex;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        menuButtonController.index = thisIndex;
+    }
 
-        if(menuButtonController.index == thisIndex)
+    // Update is called once per frame
+    void Update () {
+
+        if (menuButtonController.index == thisIndex)
         {
             animator.SetBool("selected", true);
+
+            if (Input.GetAxis("Submit") == 1)
+                animator.SetBool("pressed", true);
+            else if (animator.GetBool("pressed"))
+                animator.SetBool("pressed", false);
         }
         else
         {
             animator.SetBool("selected", false);
         }
-        
 
 	}
 }
